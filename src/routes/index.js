72da507360c9df941699d1957afd576974fda17b
quangthai <quangthai05122001@gmail.com>
2023-08-25 22:59:00 +0700

    config router/layout

---
+++ src/routes/index.js
@@ -0,0 +1,38 @@
+import { HeaderOnly } from '~/components/Layout';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+// Public Routes don't need to login
+const publicRoutes = [
+    {
+        path: '/',
+        component: Home,
+    },
+    {
+        path: '/following',
+        component: Following,
+    },
+    {
+        path: '/profile',
+        component: Profile,
+    },
+    {
+        path: '/upload',
+        component: Upload,
+        layout: HeaderOnly,
+    },
+    {
+        path: '/search',
+        component: Search,
+        layout: null,
+    },
+];
+
+// Private Routes need to login
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
