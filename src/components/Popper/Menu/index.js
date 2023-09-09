8cd4be9f0098bfd9fe4af6d6066fee90ead8804d
quangthai <quangthai05122001@gmail.com>
2023-09-09 16:32:06 +0700

    update header's ui

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -1,30 +1,65 @@
 import classNames from 'classnames/bind';
 import Tippy from '@tippyjs/react/headless';
+
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './Menu.module.scss';
+import Header from './Header';
 import MenuItem from './MenuItem';
+import { useState } from 'react';
+import { prefix } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
-function Menu({ children, items = [] }) {
-    const renderItem = () => {
-        return items.map((item, index) => <MenuItem key={index} data={item} />);
-    };
+const defaultFn = () => {};
 
-    return (
-        <Tippy
-            interactive
-            delay={[0, 500]}
-            placement="bottom-end"
-            render={(attrs) => (
-                <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
-                    <PopperWrapper className={cx('menu-popper')}>{renderItem()}</PopperWrapper>
-                </div>
+function Menu({ children, items = [], onChange = defaultFn }) {
+  const [history, setHistory] = useState([{ data: items }]);
+  const current = history[history.length - 1];
+
+  const renderItem = () => {
+    return current.data.map((item, index) => {
+      const isParent = !!item.children;
+
+      return (
+        <MenuItem
+          key={index}
+          data={item}
+          onClick={() => {
+            if (isParent) {
+              setHistory((prev) => [...prev, item.children]);
+            } else {
+              onChange(item);
+            }
+          }}
+        />
+      );
+    });
+  };
+
+  return (
+    <Tippy
+      interactive
+      delay={[0, 500]}
+      placement="bottom-end"
+      render={(attrs) => (
+        <div className={cx('menu-list')} tabIndex="-1" {...attrs}>
+          <PopperWrapper className={cx('menu-popper')}>
+            {history.length > 1 && (
+              <Header
+                title="Language"
+                onBack={() => {
+                  setHistory((prev) => prev.slice(0, prev.length - 1));
+                }}
+              />
             )}
-        >
-            {children}
-        </Tippy>
-    );
+            {renderItem()}
+          </PopperWrapper>
+        </div>
+      )}
+    >
+      {children}
+    </Tippy>
+  );
 }
 
 export default Menu;
