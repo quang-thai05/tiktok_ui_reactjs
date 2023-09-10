7dbdcba9da298d3ae1bf177fd5333c399ee36df8
quangthai <quangthai05122001@gmail.com>
2023-09-10 16:22:40 +0700

    update logic header

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,27 +1,25 @@
 import classNames from 'classnames/bind';
-import styles from './AccountItem.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+
+import styles from './AccountItem.module.scss';
 import Image from '~/components/Image';
 
 const cx = classNames.bind(styles);
 
-function AccountItem() {
+function AccountItem({ data }) {
   return (
-    <div className={cx('wrapper')}>
-      <Image
-        className={cx('avatar')}
-        src="https://p9-sign-sg.tiktokcdn.com/aweme/100x100/tiktok-obj/1601759793852418.jpeg?x-expires=1694422800&x-signature=9mQXWvigSNzim0Hf3WA9D78WVLQ%3D"
-        alt=""
-      />
+    <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+      <Image className={cx('avatar')} src={data.avatar} alt={data.avatar} />
       <div className={cx('info')}>
         <h4 className={cx('name')}>
-          <span>Nguyen Van A</span>
-          <FontAwesomeIcon className={cx('check-icon')} icon={faCheckCircle} />
+          <span>{data.full_name}</span>
+          {data.tick && <FontAwesomeIcon className={cx('check-icon')} icon={faCheckCircle} />}
         </h4>
-        <span className={cx('username')}>nguyenvana</span>
+        <span className={cx('username')}>{data.nickname}</span>
       </div>
-    </div>
+    </Link>
   );
 }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,28 +8,11 @@ import Search from '~/pages/Search';
 
 // Public Routes don't need to login
 const publicRoutes = [
-    {
-        path: '/',
-        component: Home,
-    },
-    {
-        path: '/following',
-        component: Following,
-    },
-    {
-        path: '/profile',
-        component: Profile,
-    },
-    {
-        path: '/upload',
-        component: Upload,
-        layout: HeaderOnly,
-    },
-    {
-        path: '/search',
-        component: Search,
-        layout: null,
-    },
+  { path: '/', component: Home },
+  { path: '/following', component: Following },
+  { path: '/:nickname', component: Profile },
+  { path: '/upload', component: Upload, layout: HeaderOnly },
+  { path: '/search', component: Search, layout: null },
 ];
 
 // Private Routes need to login
