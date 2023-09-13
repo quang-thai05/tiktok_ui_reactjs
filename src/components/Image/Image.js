e65e01ac5434585326e979637765a67cd6e4e9f7
quangthai <quangthai05122001@gmail.com>
2023-09-13 16:27:10 +0700

    update src code

---
+++ src/components/Image/Image.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import { useState, forwardRef } from 'react';
+import classNames from 'classnames/bind';
+import images from '~/assets/images';
+import styles from './Image.module.scss';
+
+function Image({ src, alt, className, fallback: customFallback = images.noImg, ...props }, ref) {
+  const [fallback, setFallback] = useState('');
+
+  const handleError = () => {
+    setFallback(customFallback);
+  };
+
+  return (
+    <img
+      className={classNames(styles.wrapper, className)}
+      ref={ref}
+      src={fallback || src}
+      alt={alt}
+      {...props}
+      onError={handleError}
+    />
+  );
+}
+
+Image.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+  className: PropTypes.string,
+  fallback: PropTypes.string,
+};
+
+export default forwardRef(Image);
